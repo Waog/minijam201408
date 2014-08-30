66ebe4e193253a3c839c67c687995771ee676ef7
Waog <oliver.stadie@gmail.com>
2014-08-30 18:56:26 +0200

    added player movement

--- www/js/minijam.js
+++ www/js/minijam.js
@@ -125,6 +125,7 @@ var GameBp;
         __extends(GameScene, _super);
         function GameScene() {
             _super.apply(this, arguments);
+            this.MAX_SPEED = 500;
         }
         GameScene.prototype.preload = function () {
             this.load.tilemap('map', 'assets/map_00.json', null, Phaser.Tilemap.TILED_JSON);
@@ -155,17 +156,87 @@ var GameBp;
             var ground = map.createLayer('ground');
             var red = map.createLayer('red');
             var green = map.createLayer('green');
+            var green = map.createLayer('objects');
 
-            var tutorialString = "shoot the black guy,\ndon't shot the white guy.";
+            var tutorialString = "collect the colors, reach the goal!";
             this.game.add.bitmapText(10, 10, 'bmFont', tutorialString, 50);
 
             var enemy = this.add.sprite(100, 100, "enemy");
             this.addPhysicsMovmentAndColision(enemy);
             this.addInputHandler(enemy, this.onWin);
 
-            var friend = this.add.sprite(100, 100, "friend");
-            this.addPhysicsMovmentAndColision(friend);
-            this.addInputHandler(friend, this.onLose);
+            this.player = this.add.sprite(100, 100, "friend");
+            this.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+            // Capture certain keys to prevent their default actions in the browser.
+            // This is only necessary because this is an HTML5 game. Games on other
+            // platforms may not need code like this.
+            this.input.keyboard.addKeyCapture([
+                Phaser.Keyboard.LEFT,
+                Phaser.Keyboard.RIGHT,
+                Phaser.Keyboard.UP,
+                Phaser.Keyboard.DOWN
+            ]);
+        };
+
+        GameScene.prototype.update = function () {
+            if (this.leftInputIsActive()) {
+                // If the LEFT key is down, set the player velocity to move left
+                this.player.body.velocity.x = -this.MAX_SPEED;
+            } else if (this.rightInputIsActive()) {
+                // If the RIGHT key is down, set the player velocity to move right
+                this.player.body.velocity.x = this.MAX_SPEED;
+            } else {
+                // Stop the player from moving horizontally
+                this.player.body.velocity.x = 0;
+            }
+
+            if (this.upInputIsActive()) {
+                // If the RIGHT key is down, set the player velocity to move right
+                this.player.body.velocity.y = -this.MAX_SPEED;
+            } else if (this.downInputIsActive()) {
+                // If the RIGHT key is down, set the player velocity to move right
+                this.player.body.velocity.y = this.MAX_SPEED;
+            } else {
+                // Stop the player from moving horizontally
+                this.player.body.velocity.y = 0;
+            }
+        };
+
+        GameScene.prototype.leftInputIsActive = function () {
+            var isActive = false;
+
+            isActive = this.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+            isActive |= (this.game.input.activePointer.isDown && this.input.activePointer.x < this.game.width / 4);
+
+            return isActive;
+        };
+
+        GameScene.prototype.rightInputIsActive = function () {
+            var isActive = false;
+
+            isActive = this.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+            isActive |= (this.game.input.activePointer.isDown && this.input.activePointer.x > this.game.width / 2 + this.game.width / 4);
+
+            return isActive;
+        };
+
+        GameScene.prototype.upInputIsActive = function () {
+            var isActive = false;
+
+            isActive = this.input.keyboard.isDown(Phaser.Keyboard.UP);
+            isActive |= (this.game.input.activePointer.isDown && this.input.activePointer.x > this.game.width / 2 + this.game.width / 4);
+
+            return isActive;
+        };
+
+        GameScene.prototype.downInputIsActive = function () {
+            var isActive = false;
+
+            isActive = this.input.keyboard.isDown(Phaser.Keyboard.DOWN);
+            isActive |= (this.game.input.activePointer.isDown && this.input.activePointer.x > this.game.width / 2 + this.game.width / 4);
+
+            return isActive;
         };
 
         GameScene.prototype.addPhysicsMovmentAndColision = function (sprite) {
